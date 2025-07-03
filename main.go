d75c46f3c6130a625d21242f7a1f42062c022211
Barath Vedantham <vedant.barath@gmail.com>
2025-07-03 10:51:44 -0700

    added cmdAdd function to stage files and implemented splitLines utility

--- main.go
+++ main.go
@@ -29,6 +29,58 @@ func cmdInit() {
 	fmt.Println("Initialized empty fool repository in .fool/")
 }
 
+func cmdAdd(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: fool add <file>")
+		return
+	}
+	file := args[0]
+	if _, err := os.Stat(file); err != nil {
+		fmt.Printf("File '%s' does not exist.\n", file)
+		return
+	}
+	indexPath := ".fool/index"
+	var staged []string
+	if data, err := os.ReadFile(indexPath); err == nil {
+		lines := string(data)
+		for _, line := range splitLines(lines) {
+			if line == file {
+				fmt.Printf("File '%s' is already staged.\n", file)
+				return
+			}
+			if line != "" {
+				staged = append(staged, line)
+			}
+		}
+	}
+	staged = append(staged, file)
+	f, err := os.OpenFile(indexPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error updating index:", err)
+		return
+	}
+	defer f.Close()
+	for _, s := range staged {
+		fmt.Fprintln(f, s)
+	}
+	fmt.Printf("Added '%s' to staging area.\n", file)
+}
+
+func splitLines(s string) []string {
+	var lines []string
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			lines = append(lines, s[start:i])
+			start = i + 1
+		}
+	}
+	if start < len(s) {
+		lines = append(lines, s[start:])
+	}
+	return lines
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		printUsage()
@@ -39,7 +91,7 @@ func main() {
 	case "init":
 		cmdInit()
 	case "add":
-		fmt.Println("add: not implemented yet")
+		cmdAdd(os.Args[2:])
 	case "commit":
 		fmt.Println("commit: not implemented yet")
 	case "log":
